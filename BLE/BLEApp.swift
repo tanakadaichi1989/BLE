a449424fb29854ed21da0eb43e3266e54d040494
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-11-21 18:22:45 +0900

    Initial Commit

---
+++ BLE/BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEApp.swift
+//  BLE
+//
+//  Created by 田中大地 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
